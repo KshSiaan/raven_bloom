63f6c57586c5386fbeade8db638691954461a916
KshSiaan <kshsiaan@gmail.com>
2024-10-20 10:39:20 +0600

    update

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,29 +1,45 @@
+"use client";
 import Footer from "@/components/ui/footer";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, House } from "lucide-react";
-
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { useRouter } from "next/navigation";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
   return (
     <>
       <header className="p-2">
         <nav className="w-full h-auto py-1 px-4 flex flex-row justify-between items-center">
           <div className=""></div>
           <h2 className="font-bold text-4xl text-center">RavenBloom</h2>
-          <div className=""></div>
+          <div className="">
+            <ModeToggle />
+          </div>
         </nav>
         <div className="flex flex-row flex-wrap justify-between items-center">
           <div className="">
-            <Button variant="ghost">
+            <Button
+              variant="ghost"
+              onClick={() => {
+                router.back();
+              }}
+            >
               <ArrowLeft className="mr-2 h-4 w-4" />
               Go back
             </Button>
           </div>
           <div className="">
-            <Button variant="ghost">
+            <Button
+              variant="ghost"
+              onClick={() => {
+                router.replace("/");
+              }}
+            >
               <House className="mr-2 h-4 w-4" />
               Home
             </Button>
--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -3,19 +3,34 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { useAppSelector } from "@/hooks/redux-hooks";
 import { useEffect, useState } from "react";
-// import Image from "next/image";
 
 export default function Page() {
   const inventory = useAppSelector((state) => state.inventory.inventory);
   const [isClient, setIsClient] = useState(false);
+  const [total, setTotal] = useState<number>(0);
+  const [subtotal, setSubtotal] = useState<number>(0);
 
   useEffect(() => {
     setIsClient(true);
+    const totalAmount = inventory.reduce(
+      (acc, element) => acc + element.ammount,
+      0
+    );
+    setTotal(totalAmount);
+
+    //subtotal amount
+
+    const subTotalVar = inventory.reduce(
+      (acc, element) => acc + element.price,
+      0
+    );
+    setSubtotal(subTotalVar);
   }, []);
 
   return (
@@ -23,17 +38,16 @@ export default function Page() {
       <div className="col-span-4 h-dvh w-full border rounded-sm"></div>
       <Card className="col-span-2 rounded-sm">
         <CardHeader className="border-b pb-4">
-
-            <CardTitle className="text-xl uppercase">Cart Summary</CardTitle>
+          <CardTitle className="text-xl uppercase">Cart Summary</CardTitle>
           <CardDescription>
             Review your selected items before proceeding to checkout.
           </CardDescription>
         </CardHeader>
-        <CardContent className="w-full h-full py-6 flex flex-col justify-start items-start gap-y-6">
+        <CardContent className="w-full h-auto flex flex-col justify-start items-start border-b">
           {isClient && inventory && inventory.length > 0 ? (
             inventory.map((item, index) => (
               <div
-                className="grid grid-cols-10 gap-4 w-full h-full"
+                className="grid grid-cols-10 gap-4 w-full h-auto pt-6"
                 key={index}
               >
                 <div className="col-span-1 w-full h-full text-xl flex justify-start items-start">
@@ -42,13 +56,10 @@ export default function Page() {
                 <div className="col-span-9 w-full h-full">
                   <div className="w-full flex justify-between items-start font-semibold">
                     <span>{item.name}</span>
-                    <span>{item.price}</span>
+                    <span>${item.price}.00</span>
                   </div>
-                  <p className="text-zinc-400 text-sm pr-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Dolorum sequi quod odio, earum iste culpa voluptate! Facere
-                    reiciendis asperiores, deserunt, eius excepturi ex
-                    molestias, sequi et commodi maiores quaerat animi.
+                  <p className="text-zinc-600 dark:text-zinc-400 text-sm pr-4">
+                    {item.description}
                   </p>
                 </div>
               </div>
@@ -56,7 +67,28 @@ export default function Page() {
           ) : (
             <p className="text-zinc-500">Your cart is empty</p>
           )}
+          <div className="w-full border-b my-4" />
+          <div className="w-full flex justify-between items-center text-md font-semibold py-2">
+            <div className="">Subtotal({total}):</div>
+            <div className="">${subtotal}.00</div>
+          </div>
+
+          <div className="w-full flex justify-between items-center text-md font-semibold py-2">
+            <div className="">Est. Delivery and Setup</div>
+            <div className="">.Included</div>
+          </div>
+          <div className="w-full flex justify-between items-center text-md font-semibold py-2">
+            <div className="">Estimated Tax.</div>
+            <div className="">--</div>
+          </div>
+          <div className="h-[30px] w-full" />
         </CardContent>
+        <CardFooter>
+          <div className="w-full flex justify-between items-center text-md font-semibold py-2">
+            <div className="">Estimated Total:</div>
+            <div className="">${subtotal}.00</div>
+          </div>
+        </CardFooter>
       </Card>
     </div>
   );
